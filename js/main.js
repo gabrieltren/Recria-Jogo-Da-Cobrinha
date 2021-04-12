1d930f4f223100d10a4b0bb9ba394b35e1eb0d9f
Gabriel Trentini <gabriel.silveira1207@gmail.com>
2021-04-12 00:31:39 -0300

    Definido os movimentos e adcionando direçoes para movimento

--- js/main.js
+++ js/main.js
@@ -12,6 +12,7 @@ function criarBG() {
     context.fillStyle = "lightgreen";
     context.fillRect( 0, 0, 16 * box, 16 * box);
 }
+
 function criarCobra(){
     for(i=0; i<snake.length; i++){
         if(i == 0){
@@ -25,7 +26,20 @@ function criarCobra(){
     }
 }
 
+document.addEventListener("keydown", update);
+
+function update(event){
+    if(event.keyCode == 37 && direction != "right" )  direction = "left";
+    if(event.keyCode == 38 && direction != "down" ) direction = "up";
+    if(event.keyCode == 39 && direction != "left" ) direction = "right";
+    if(event.keyCode == 40 && direction != "up") direction = "down";
+}
+
 function iniciarJogo(){
+    if(snake[0].x > 15*box && direction == "right") snake[0].x = 0;
+    if(snake[0].x < 0 && direction == "left") snake[0].x = 15*box;
+    if(snake[0].y > 15*box && direction == "down") snake[0].y = 0;
+    if(snake[0].y < 0 && direction == "up") snake[0].y = 15*box;
     criarBG();
     criarCobra();
 
@@ -43,8 +57,8 @@ function iniciarJogo(){
         y: snakeY,
     }
     snake.unshift(newHead);
-
+    
 }
 
-let jogo = setInterval(iniciarJogo, 100);
+let jogo = setInterval(iniciarJogo, 200);
 
